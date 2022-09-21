b2c13ac0bb016be72720911d720538217cf5b34a
Mikael Souza <mikael.souza.cc@gmail.com>
2022-09-20 22:43:43 -0400

    asdasdsad

--- src/functions.rs
+++ src/functions.rs
@@ -1,12 +1,18 @@
 pub fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
+
 mod tests {
-    use super::sum;
     #[test]
     fn test_sum() {
         let expected = 4;
         let result = super::sum(2, 2);
         assert_eq!(expected, result);
+    }
+    #[test]
+    fn test_sum_err() {
+        let expected = 5;
+        let result = super::sum(2, 2);
+        assert_ne!(expected, result);
     }
 }
